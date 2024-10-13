93fe68d95e5f7a59fb0cf6173dbfbcde1d6dcef8
ryder <pereiramatias555@gmail.com>
2024-10-12 23:52:02 -0300

    update menu

--- Gestion_Mecanica/Menu.Designer.vb
+++ Gestion_Mecanica/Menu.Designer.vb
@@ -23,6 +23,7 @@ Partial Class Menu
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Panel1 = New Panel()
+        buttonHome = New Button()
         buttonRepuestos = New Button()
         buttonGestionServicios = New Button()
         buttonSoliServicios = New Button()
@@ -34,12 +35,19 @@ Partial Class Menu
         Label3 = New Label()
         Label4 = New Label()
         Panel2 = New Panel()
+        panelHome = New Panel()
+        panelVentaRepuestos = New Panel()
+        panelSiniestro = New Panel()
+        panelGestionServicios = New Panel()
+        panelSoliServicios = New Panel()
+        panelRepuestos = New Panel()
         Panel1.SuspendLayout()
         Panel2.SuspendLayout()
         SuspendLayout()
         ' 
         ' Panel1
         ' 
+        Panel1.Controls.Add(buttonHome)
         Panel1.Controls.Add(buttonRepuestos)
         Panel1.Controls.Add(buttonGestionServicios)
         Panel1.Controls.Add(buttonSoliServicios)
@@ -51,47 +59,56 @@ Partial Class Menu
         Panel1.Size = New Size(796, 48)
         Panel1.TabIndex = 0
         ' 
+        ' buttonHome
+        ' 
+        buttonHome.Location = New Point(0, 0)
+        buttonHome.Name = "buttonHome"
+        buttonHome.Size = New Size(94, 48)
+        buttonHome.TabIndex = 6
+        buttonHome.Text = "Home"
+        buttonHome.UseVisualStyleBackColor = True
+        ' 
         ' buttonRepuestos
         ' 
-        buttonRepuestos.Location = New Point(0, 0)
+        buttonRepuestos.Location = New Point(91, 0)
         buttonRepuestos.Name = "buttonRepuestos"
-        buttonRepuestos.Size = New Size(135, 48)
+        buttonRepuestos.Size = New Size(116, 48)
         buttonRepuestos.TabIndex = 5
         buttonRepuestos.Text = "Respuestos"
         buttonRepuestos.UseVisualStyleBackColor = True
         ' 
         ' buttonGestionServicios
         ' 
-        buttonGestionServicios.Location = New Point(574, 0)
+        buttonGestionServicios.Location = New Point(593, 0)
         buttonGestionServicios.Name = "buttonGestionServicios"
-        buttonGestionServicios.Size = New Size(152, 48)
+        buttonGestionServicios.Size = New Size(133, 48)
         buttonGestionServicios.TabIndex = 4
         buttonGestionServicios.Text = "Gestion - solicitudes"
         buttonGestionServicios.UseVisualStyleBackColor = True
         ' 
         ' buttonSoliServicios
         ' 
-        buttonSoliServicios.Location = New Point(426, 0)
+        buttonSoliServicios.Location = New Point(464, 0)
         buttonSoliServicios.Name = "buttonSoliServicios"
-        buttonSoliServicios.Size = New Size(152, 48)
+        buttonSoliServicios.Size = New Size(133, 48)
         buttonSoliServicios.TabIndex = 3
         buttonSoliServicios.Text = "Solicitud de servicio"
         buttonSoliServicios.UseVisualStyleBackColor = True
         ' 
         ' buttonSiniestros
         ' 
-        buttonSiniestros.Location = New Point(279, 0)
+        buttonSiniestros.Location = New Point(334, 0)
         buttonSiniestros.Name = "buttonSiniestros"
-        buttonSiniestros.Size = New Size(152, 48)
+        buttonSiniestros.Size = New Size(133, 48)
         buttonSiniestros.TabIndex = 2
         buttonSiniestros.Text = "Siniestros"
         buttonSiniestros.UseVisualStyleBackColor = True
         ' 
         ' buttonVentaRepuestos
         ' 
-        buttonVentaRepuestos.Location = New Point(131, 0)
+        buttonVentaRepuestos.Location = New Point(204, 0)
         buttonVentaRepuestos.Name = "buttonVentaRepuestos"
-        buttonVentaRepuestos.Size = New Size(152, 48)
+        buttonVentaRepuestos.Size = New Size(133, 48)
         buttonVentaRepuestos.TabIndex = 1
         buttonVentaRepuestos.Text = "Venta-Repuestos"
         buttonVentaRepuestos.UseVisualStyleBackColor = True
@@ -158,14 +175,66 @@ Partial Class Menu
         Panel2.Size = New Size(796, 29)
         Panel2.TabIndex = 5
         ' 
+        ' panelHome
+        ' 
+        panelHome.BackgroundImage = My.Resources.Resources.fondomotor
+        panelHome.BackgroundImageLayout = ImageLayout.Stretch
+        panelHome.Location = New Point(8, 57)
+        panelHome.Name = "panelHome"
+        panelHome.Size = New Size(790, 392)
+        panelHome.TabIndex = 6
+        ' 
+        ' panelVentaRepuestos
+        ' 
+        panelVentaRepuestos.Location = New Point(8, 57)
+        panelVentaRepuestos.Name = "panelVentaRepuestos"
+        panelVentaRepuestos.Size = New Size(790, 392)
+        panelVentaRepuestos.TabIndex = 1
+        ' 
+        ' panelSiniestro
+        ' 
+        panelSiniestro.Location = New Point(8, 57)
+        panelSiniestro.Name = "panelSiniestro"
+        panelSiniestro.Size = New Size(790, 392)
+        panelSiniestro.TabIndex = 2
+        ' 
+        ' panelGestionServicios
+        ' 
+        panelGestionServicios.Location = New Point(8, 57)
+        panelGestionServicios.Name = "panelGestionServicios"
+        panelGestionServicios.Size = New Size(790, 392)
+        panelGestionServicios.TabIndex = 4
+        ' 
+        ' panelSoliServicios
+        ' 
+        panelSoliServicios.Location = New Point(8, 57)
+        panelSoliServicios.Name = "panelSoliServicios"
+        panelSoliServicios.Size = New Size(790, 392)
+        panelSoliServicios.TabIndex = 3
+        ' 
+        ' panelRepuestos
+        ' 
+        panelRepuestos.Location = New Point(8, 57)
+        panelRepuestos.Name = "panelRepuestos"
+        panelRepuestos.Size = New Size(790, 392)
+        panelRepuestos.TabIndex = 0
+        ' 
         ' Menu
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = My.Resources.Resources.fondoLogin01
         ClientSize = New Size(800, 478)
+        Controls.Add(panelSiniestro)
+        Controls.Add(panelVentaRepuestos)
+        Controls.Add(panelRepuestos)
+        Controls.Add(panelSoliServicios)
+        Controls.Add(panelGestionServicios)
+        Controls.Add(panelHome)
         Controls.Add(Panel2)
         Controls.Add(Panel1)
         Name = "Menu"
+        StartPosition = FormStartPosition.CenterScreen
         Text = "Menu"
         Panel1.ResumeLayout(False)
         Panel2.ResumeLayout(False)
@@ -185,4 +254,11 @@ Partial Class Menu
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
     Friend WithEvents Panel2 As Panel
+    Friend WithEvents buttonHome As Button
+    Friend WithEvents panelHome As Panel
+    Friend WithEvents panelSoliServicios As Panel
+    Friend WithEvents panelSiniestro As Panel
+    Friend WithEvents panelVentaRepuestos As Panel
+    Friend WithEvents panelRepuestos As Panel
+    Friend WithEvents panelGestionServicios As Panel
 End Class
