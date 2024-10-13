cc81fc64fc8d0fc4585699e9b2c5304aea3796c4
ryder <pereiramatias555@gmail.com>
2024-10-12 21:45:08 -0300

    login

--- Gestion_Mecanica/Login.Designer.vb
+++ Gestion_Mecanica/Login.Designer.vb
@@ -22,9 +22,155 @@ Partial Class Login
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Login"
+        tituloLogin = New Label()
+        PictureBox1 = New PictureBox()
+        labelUsuario = New Label()
+        textboxUsuario = New TextBox()
+        labelContrasena = New Label()
+        textboxContrasena = New TextBox()
+        Panel1 = New Panel()
+        Panel2 = New Panel()
+        Panel3 = New Panel()
+        buttonLogin = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        Panel1.SuspendLayout()
+        Panel2.SuspendLayout()
+        Panel3.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' tituloLogin
+        ' 
+        tituloLogin.AutoSize = True
+        tituloLogin.BackColor = Color.Transparent
+        tituloLogin.Font = New Font("Segoe UI Black", 36F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        tituloLogin.Location = New Point(65, 9)
+        tituloLogin.Name = "tituloLogin"
+        tituloLogin.Size = New Size(160, 65)
+        tituloLogin.TabIndex = 0
+        tituloLogin.Text = "Login"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = Color.Transparent
+        PictureBox1.Image = My.Resources.Resources.llaveIcon
+        PictureBox1.Location = New Point(88, 77)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(100, 94)
+        PictureBox1.TabIndex = 1
+        PictureBox1.TabStop = False
+        ' 
+        ' labelUsuario
+        ' 
+        labelUsuario.AutoSize = True
+        labelUsuario.BackColor = SystemColors.MenuHighlight
+        labelUsuario.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        labelUsuario.ForeColor = Color.FromArgb(CByte(64), CByte(0), CByte(64))
+        labelUsuario.Location = New Point(0, 2)
+        labelUsuario.Name = "labelUsuario"
+        labelUsuario.Size = New Size(65, 21)
+        labelUsuario.TabIndex = 2
+        labelUsuario.Text = "Usuario"
+        ' 
+        ' textboxUsuario
+        ' 
+        textboxUsuario.Location = New Point(0, 36)
+        textboxUsuario.Name = "textboxUsuario"
+        textboxUsuario.Size = New Size(221, 23)
+        textboxUsuario.TabIndex = 3
+        ' 
+        ' labelContrasena
+        ' 
+        labelContrasena.AutoSize = True
+        labelContrasena.BackColor = SystemColors.Highlight
+        labelContrasena.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        labelContrasena.ForeColor = Color.FromArgb(CByte(64), CByte(0), CByte(64))
+        labelContrasena.Location = New Point(1, 2)
+        labelContrasena.Name = "labelContrasena"
+        labelContrasena.Size = New Size(92, 21)
+        labelContrasena.TabIndex = 4
+        labelContrasena.Text = "Contraseña"
+        ' 
+        ' textboxContrasena
+        ' 
+        textboxContrasena.Location = New Point(1, 39)
+        textboxContrasena.Name = "textboxContrasena"
+        textboxContrasena.PasswordChar = "*"c
+        textboxContrasena.Size = New Size(221, 23)
+        textboxContrasena.TabIndex = 5
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackgroundImage = My.Resources.Resources.fondomotor
+        Panel1.BackgroundImageLayout = ImageLayout.Stretch
+        Panel1.BorderStyle = BorderStyle.Fixed3D
+        Panel1.Controls.Add(Panel2)
+        Panel1.Controls.Add(Panel3)
+        Panel1.Location = New Point(12, 177)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(262, 186)
+        Panel1.TabIndex = 6
+        ' 
+        ' Panel2
+        ' 
+        Panel2.BackColor = Color.MediumSpringGreen
+        Panel2.Controls.Add(textboxUsuario)
+        Panel2.Controls.Add(labelUsuario)
+        Panel2.Location = New Point(17, 20)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(222, 60)
+        Panel2.TabIndex = 8
+        ' 
+        ' Panel3
+        ' 
+        Panel3.BackColor = Color.MediumSpringGreen
+        Panel3.Controls.Add(textboxContrasena)
+        Panel3.Controls.Add(labelContrasena)
+        Panel3.Location = New Point(16, 98)
+        Panel3.Name = "Panel3"
+        Panel3.Size = New Size(225, 64)
+        Panel3.TabIndex = 7
+        ' 
+        ' buttonLogin
+        ' 
+        buttonLogin.Location = New Point(30, 379)
+        buttonLogin.Name = "buttonLogin"
+        buttonLogin.Size = New Size(233, 44)
+        buttonLogin.TabIndex = 7
+        buttonLogin.Text = "Login"
+        buttonLogin.UseVisualStyleBackColor = True
+        ' 
+        ' Login
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = My.Resources.Resources.fondoLogin01
+        ClientSize = New Size(286, 450)
+        Controls.Add(buttonLogin)
+        Controls.Add(PictureBox1)
+        Controls.Add(tituloLogin)
+        Controls.Add(Panel1)
+        ForeColor = Color.DodgerBlue
+        Name = "Login"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Login"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        Panel1.ResumeLayout(False)
+        Panel2.ResumeLayout(False)
+        Panel2.PerformLayout()
+        Panel3.ResumeLayout(False)
+        Panel3.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents tituloLogin As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents labelUsuario As Label
+    Friend WithEvents textboxUsuario As TextBox
+    Friend WithEvents labelContrasena As Label
+    Friend WithEvents textboxContrasena As TextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents buttonLogin As Button
 End Class
--- Gestion_Mecanica/Login.vb
+++ Gestion_Mecanica/Login.vb
@@ -1,3 +1,39 @@
-﻿Public Class Login
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Login
+
+    Private Sub buttonLogin_Click(sender As Object, e As EventArgs) Handles buttonLogin.Click
+        ' Cadena de conexión a MySQL sin usuario ni contraseña
+        Dim connectionString As String = "server=localhost;database=taller;uid=root;pwd=;"
+
+        ' Abrir conexión a la base de datos
+        Using connection As New MySqlConnection(connectionString)
+            Try
+                connection.Open()
+                ' Consulta SQL para verificar si el correo y la contraseña coinciden con un usuario en la base de datos
+                Dim query As String = "SELECT * FROM usuarios WHERE Correo = @correo AND Contraseña = @contrasena"
+                Dim command As New MySqlCommand(query, connection)
+
+                ' Pasar los valores ingresados en los textboxes como parámetros
+                command.Parameters.AddWithValue("@correo", textboxUsuario.Text)
+                command.Parameters.AddWithValue("@contrasena", textboxContrasena.Text)
+
+                ' Ejecutar la consulta
+                Dim reader As MySqlDataReader = command.ExecuteReader()
+
+                If reader.HasRows Then
+                    MessageBox.Show("Ha iniciado sesión correctamente.")
+                Else
+                    MessageBox.Show("Usuario o contraseña no válidos.")
+                End If
+
+            Catch ex As Exception
+                MessageBox.Show("Error de conexión: " & ex.Message)
+            End Try
+        End Using
+    End Sub
+
+
+
 
 End Class
