f70535a6e6158be78e760aeb942406fda3a6772a
alumnos.republica <alumnos.republica@REP-AL-RLC22-09>
2024-10-29 20:39:45 -0300

    update login

--- Gestion_Mecanica/Login.vb
+++ Gestion_Mecanica/Login.vb
@@ -4,11 +4,12 @@ Public Class Login
 
     Private Sub buttonLogin_Click(sender As Object, e As EventArgs) Handles buttonLogin.Click
         Dim connectionString As String = "server=localhost;database=taller;uid=root;pwd=;"
-        Dim query As String = "SELECT Correo, Contraseña, Tipo FROM usuarios WHERE Correo=@correo AND Contraseña=@contraseña"
+        ' Modificación de la consulta para aceptar Correo o RUT
+        Dim query As String = "SELECT Correo, Contraseña, Tipo FROM usuarios WHERE (Correo = @usuario OR Rut = @usuario) AND Contraseña = @contraseña"
 
         Using connection As New MySqlConnection(connectionString)
             Dim command As New MySqlCommand(query, connection)
-            command.Parameters.AddWithValue("@correo", textboxUsuario.Text)
+            command.Parameters.AddWithValue("@usuario", textboxUsuario.Text)
             command.Parameters.AddWithValue("@contraseña", textboxContrasena.Text)
 
             connection.Open()
